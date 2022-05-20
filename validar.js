ae4f9381d186c7102ec8769cb35cecaf5df1368c
Juan Caminos <jcz_juan@hotmail.com>
2022-05-20 18:47:20 -0300

    form validado

--- validar.js
+++ validar.js
@@ -1,13 +1,25 @@
 function valida_enviaev() {
-    //valido telefono, tiene que ser entero
+    //validación de nombre
+    if (document.fevento.nombre.value.length == 0) {
+        alert("Completar nombre")
+        document.fevento.nombre.focus()
+        return 0;
+    }
+    //validación de email
+    if (document.fevento.mail.value.length == 0) {
+        alert("Completar email")
+        document.fevento.mail.focus()
+        return 0;
+    }
+    //validación de telefono, tiene que ser entero
     numero = document.fevento.numero.value
     numero = validarEntero(numero)
     document.fevento.numero.value = numero
     if (numero == "") {
-        alert("Tiene que introducir un número de teléfono")
+        alert("Introducir un número de teléfono")
         document.fevento.numero.focus()
-    return 0;}
-
+    return 0;
+    }
     //envio de formulario
     alert("Muchas gracias por escribirnos");
     document.fevento.submit();
@@ -23,6 +35,18 @@ function validarEntero(valor) {
 }
 
 function valida_enviaop() {
+    //valido el nombre
+    if (document.fopinion.nombre.value.length == 0) {
+        alert("Completar nombre")
+        document.fopinion.nombre.focus()
+        return 0;
+    }
+    //valido el email
+    if (document.fopinion.mail.value.length == 0) {
+        alert("Completar email")
+        document.fopinion.mail.focus()
+        return 0;
+    }
     //envio de formulario
     alert("Muchas gracias por dejarnos tu opinion");
     document.fopinion.submit();
